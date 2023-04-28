67d49286c7b8207fb6a72e4d50ea9dbe21cd4092
Zhipeng <mrzpding@gmail.com>
2023-04-28 00:09:28 -0500

    fix bugs about validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   paginates_per 9
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :on_sale_price, numericality: { greater_than_or_equal_to: 0, less_than: :price }, allow_nil: true
+  validates :on_sale_price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: :price }, allow_nil: true
   validates :description, presence: true
   has_one_attached :image
 end
